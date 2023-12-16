2854d1396f33a32e9a6f80d06594e3fe4edf05b9
I Love Sapujagad <onetheworst27@gmail.com>
2023-12-16 20:31:00 +0700

    chore: add route

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import App from '../containers/App'
+import React from "react";
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "../containers/App";
+import Test from "../containers/Test";
 
 const Root = ({ store }) => (
-    <Provider store={store}>
-        <Router>
-            <Route path="/:filter?" component={App} />
-        </Router>
-    </Provider>
-)
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route path="/:filter?" component={App} />
+        <Route exact path="/test" component={Test} />
+      </div>
+    </Router>
+  </Provider>
+);
 
 Root.propTypes = {
-    store: PropTypes.object.isRequired
-}
+  store: PropTypes.object.isRequired,
+};
 
-export default Root
+export default Root;
